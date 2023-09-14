3a29b8cb230e6635e3fdb25296d51cc940ebe0a4
blemathegreat <blemathegreat@gmail.com>
2023-09-14 05:16:10 -0700

    main

---
+++ positive_or_negative.c
@@ -0,0 +1,9 @@
+#include 'main.h
+void  positive_or_negative(int i)
+{
+	if (i = 0)
+		printf("%d is positive\n",);
+	elseif (i == 0)
+		ptintf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
